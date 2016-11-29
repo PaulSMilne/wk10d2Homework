0b33ea60d8ff481dac6788fcaa01b8ecf6bcc6cc
Paul Milne <hallhill@gmail.com>
2016-11-29 19:04:01 +0000

    successfully wrote and tested a method to remove a specific item from the list

--- basket.js
+++ basket.js
@@ -4,6 +4,10 @@ var shoppingBasket = {
      purchases: [],
      add: function(item){
           this.purchases.push(item);
+     },
+     remove: function(item){
+          var index = this.purchases.indexOf(item);
+          this.purchases.splice(index, 1);
      }
 }
 
--- specs/basket_spec.js
+++ specs/basket_spec.js
@@ -1,7 +1,8 @@
 var basket = require('../basket');
 var assert = require('assert');
-var item1 = require('../item')
-var item2 = require('../item')
+var item1 = require('../item');
+var item2 = require('../item');
+var item3 = require('../item');
 
 describe('List', function(){
      it('should be an empty array at start', function(){
@@ -10,10 +11,19 @@ describe('List', function(){
      it('can have an item added', function(){
           item1.setName("Kazoo");
           item1.setPrice(5);
-          item2.setName("Kite");
-          item2.setPrice("15");
+          item2.setName("Penny Whistle");
+          item2.setPrice(15);
+          item3.setName("Ocarina");
+          item3.setPrice(7);
           basket.add(item1);
           basket.add(item2);
-          assert.equal(2, basket.purchases.length);
+          basket.add(item3);
+          assert.equal(3, basket.purchases.length);
      })
+     it('can remove a specific item from the list', function(){
+          basket.remove(item2);
+          assert.equal(2, basket.purchases.length);
+     });
+
+     it('can calculate the total price of all purchases');
 })
